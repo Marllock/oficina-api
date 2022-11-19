fed677e83ee42097cf284356f91144963ef09901
root <root@DESKTOP-RCQEJOL>
2022-11-19 18:32:53 -0300

    address-update

--- src/main/java/com/oficina/educacional/domain/model/Address.java
+++ src/main/java/com/oficina/educacional/domain/model/Address.java
@@ -6,14 +6,54 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
+~/oficina/back/test/oficina-api/src/java/com/oficina/educacional/domain/model/Address.java@NoArgsConstructor
 public class Address {
-
+	@Schema(
+        description = "User Street",
+        example = "Rua Dos Andradas",
+        required = true
+    )
     private String userStreet;
+
+    @Schema(
+        description = "User Street Number",
+        example = "123",
+        required = true
+    )    
     private String userStreetNumber;
+
+    @Schema(
+        description = "User Address Complement",
+        example = "Apto 213",
+        required = false
+    )
     private String userComplement;
+
+    @Schema(
+        description = "User District",
+        example = "Downtown",
+        required = true
+    )
     private String userDistrict;
+
+    @Schema(
+        description = "User CEP",
+        example = "28960000",
+        required = true
+    )
     private String userZipCode;
+
+    @Schema(
+        description = "User State",
+        example = "Rio de Janeiro",
+        required = true
+    )
     private String userState;
+
+    @Schema(
+        description = "User City",
+        example = "Iguaba Grande",
+        required = true
+    )
     private String userCity;
 }
