f6a99e70bb9d4b543503ab0e2ad6200d178b5a49
Marllock <marcellovbenites@hotmail.com>
2022-11-19 21:07:24 -0300

    Ajustar schemas

--- src/main/java/com/oficina/educacional/api/model/DisciplineDTO.java
+++ src/main/java/com/oficina/educacional/api/model/DisciplineDTO.java
@@ -5,22 +5,49 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 public class DisciplineDTO {
 
-    private Long id;
+    @Schema(
+        description = "Discipline entity uniqe identifier",
+        example = "1",
+        required = true
+    )
+    private Long disciplineId;
 
+    @Schema(
+        description = "Discipline code",
+        example = "AS31C",
+        required = true
+    )
     @NotBlank
     private String disciplineCode;
 
+
+    @Schema(
+        description = "Discipline name",
+        example = "Estrutura de Dados",
+        required = true
+    )
     @NotBlank
     @Length(max = 25)
     private String disciplineName;
 
+    @Schema(
+        description = "Discipline name normalized",
+        example = "estrutura_de_dados",
+        required = true
+        
+    )
     @NotBlank
     @Length(max = 25)
     private String disciplineNameNormalized;
 
+    @Schema(
+        description = "Discipline Course entity",
+        required = true
+    )
     private Course disciplineCourseId;
 }
--- src/main/java/com/oficina/educacional/api/model/StudentDTO.java
+++ src/main/java/com/oficina/educacional/api/model/StudentDTO.java
@@ -3,11 +3,27 @@ package com.oficina.educacional.api.model;
 import com.oficina.educacional.domain.model.Course;
 import com.oficina.educacional.domain.model.User;
 import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 public class StudentDTO {
 
+    @Schema(
+        description = "Student unique identifier",
+        example = "1",
+        required = true
+    )
     private long studentId;
+
+    @Schema(
+        description = "Student user related entity",
+        required = true
+    )
     private User user;
+
+    @Schema(
+        description = "Student course related entity",
+        required = true
+    )
     private Course studentCourse;
 }
